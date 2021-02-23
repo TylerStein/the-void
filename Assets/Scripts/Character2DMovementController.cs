b4c5ec68c86f73f7ce82a36743c9c4eca81c7d16
= <tylers@rappid.design>
2021-02-22 21:56:33 -0500

    Initial commit

---
+++ Assets/Scripts/Character2DMovementController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+[RequireComponent(typeof(Collider2D))]
+public class Character2DMovementController : MonoBehaviour
+{
+    public Character2DMovementSettings movementSettings;
+    private new Transform transform;
+
+    public Vector2 velocity = Vector2.zero;
+    public Vector2 force = Vector2.zero;
+    public Vector2 moveInput = Vector2.zero;
+
+    public float ground = 0f;
+
+    private void Awake() {
+        transform = GetComponent<Transform>();
+    }
+
+    private void OnDisable() {
+        //
+    }
+
+    private void Update() {
+        force.x += moveInput.x * movementSettings.groundMoveForce * Time.deltaTime;
+
+        if (force.x == 0f && velocity.x != 0f) {
+            velocity.x = 0f;
+        }
+
+        force += movementSettings.gravity * Time.deltaTime;
+
+        Vector3 projectedVelocity = velocity + force * Time.deltaTime;
+        force = Vector2.zero;
+
+        projectedVelocity.x = Mathf.Clamp(projectedVelocity.x, -movementSettings.maxVelocity.x, movementSettings.maxVelocity.x);
+        projectedVelocity.y = Mathf.Clamp(projectedVelocity.y, -movementSettings.maxVelocity.y, movementSettings.maxVelocity.y);
+
+        Vector3 projectedPosition = transform.position + projectedVelocity;
+        if (projectedPosition.y < ground) {
+            projectedPosition.y = ground;
+            projectedVelocity.y = 0f;
+        }
+
+        velocity = projectedVelocity;
+        transform.position = projectedPosition;
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        force.y = movementSettings.jumpForce;
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Scripts/Character2DMovementSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character2DMovementSettings", menuName = "2D Movement Settings", order = 1)]
+public class Character2DMovementSettings : ScriptableObject
+{
+    public float groundMoveForce = 1f;
+    public float jumpForce = 3f;
+
+    public Vector2 maxVelocity = new Vector2(10f, 10f);
+
+    public Vector2 gravity = new Vector2(0f, -9.8f);
+}

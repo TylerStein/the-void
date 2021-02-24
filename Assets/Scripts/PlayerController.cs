65554ba8b11b8ba0d6271b3530bceb094b8f7c7c
= <tylers@rappid.design>
2021-02-24 09:05:36 -0500

    Sprite layering, player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
     public Character2DMovementController movement;
+    public Animator playerAnimator;
+
+    private void Update() {
+        playerAnimator.SetBool("moving", movement.MoveInput != 0);
+    }
 
     public void OnMove(InputAction.CallbackContext value) {
         movement.MoveInput = value.ReadValue<Vector2>().x;
+        if (movement.MoveInput != 0) {
+            spriteRenderer.flipX = movement.MoveInput < 0f;
+        }
     }
 
     public void OnJump(InputAction.CallbackContext value) {
         if (value.started) {
+            playerAnimator.SetBool("jumping", true);
             movement.BeginJump();
         } else if (value.canceled) {
+            playerAnimator.SetBool("jumping", false);
             movement.EndJump();
         }
     }

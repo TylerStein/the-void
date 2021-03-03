218af93a3efa19d9dfafbfe3ed82c97a05d6ed3b
= <tylers@rappid.design>
2021-03-02 20:06:47 -0500

    Added framerate controller

---
+++ Assets/Scripts/EditorFramerateController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorFramerateController : MonoBehaviour
+{
+    public int targetFrameRate = 30;
+
+    void Start()
+    {
+#if UNITY_EDITOR
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+#endif
+    }
+}
